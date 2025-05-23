5ab07f1a43b66accba4711e2757dc87e8e4e9e67
hamza <traorehamza407@gmail.com>
2025-05-23 12:53:44 +0200

    Refactor le contrôleur Article pour utiliser le repository et la pagination, amélioration de la récupération des articles publiés

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class ArticleController extends AbstractController
 {
     #[Route('/list', name: 'article', methods: ['GET'])]
     public function index(
-        ArticleController $ar, // Repository de l'entité Article
-         $paginator, // Classe pour la fonctionnalité de pagination
+        ArticleRepository $ar, // Repository de l'entité Article
+        PaginatorInterface $paginator, // Classe pour la fonctionnalité de pagination
         Request $request // Classe epour recuperer les parametres de la requete HTTP
     ): Response {
-        $all   = $ar->findAll(); // Récupération de tous les articles
+        // Récupération de tous les articles
+        $all = $ar->findBy([
+            'isPublished' => true, // On ne veut que les articles publiés
+            'isArchived' => false// On ne veut pas les articles archivés
+        ], ['title' => 'ASC'],);
         $pagination = $paginator->paginate(
             $all,
             $request->query->getInt('page', 1),
@@ -25,6 +32,32 @@ final class ArticleController extends AbstractController
         return $this->render('article/index.html.twig', [
             'controller_name' => 'INDEX',
             'articles' => $pagination
+        ]);
+    }
+    // Route "/article/{slug}" menent à la page d'un article
+    #[Route('/{slug}', name: 'article', methods: ['GET'])]
+    public function view(): Response
+    {
+        return $this->render('article/view.html.twig', [
+            //'articles' => $article
+        ]);
+    }
+
+    // Route "/article/{slug}/edit" menent à la page de modification d'un article
+    #[Route('/{slug}/edit', name: 'article_edit', methods: ['GET', 'POST'])]
+    public function edit(): Response
+    {
+        return $this->render('article/edit.html.twig', [
+            //'articles' => $article
+        ]);
+    }
+
+    // Route "/article/{slug}/delete" menent à la page de suppression d'un article
+    #[Route('/{slug}/delete', name: 'article_delete', methods: ['GET'])]
+    public function delete(): Response
+    {
+        return $this->render('article/delete.html.twig', [
+            //'articles' => $article
         ]);
     }
 }
