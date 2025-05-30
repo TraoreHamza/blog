5ed2af0b87d28f9c7a33527aa0ee9fa11e7e9735
hamza <traorehamza407@gmail.com>
2025-05-30 13:06:07 +0200

    Ajout des bundles TwigComponent et LiveComponent dans la configuration des bundles

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
