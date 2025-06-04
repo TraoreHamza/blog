45fea8f33ce368732eddc70ceb55f0e12fa8d2f7
hamza <traorehamza407@gmail.com>
2025-06-04 13:41:06 +0200

    Réduction du nombre d'utilisateurs générés dans UserFixtures de 100 à 50 et mise à jour de la méthode de hachage du mot de passe

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,18 +6,25 @@ use Faker\Factory;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $hasher
+    ){}
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
 
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 0; $i < 50; $i++) {
+
             $user = new User();
             $user
                 ->setEmail($faker->email())
-                ->setPassword($faker->password())
+                ->setUsername($faker->userName())
+                ->setPassword($this->hasher->hashPassword($user, 'admin'))
                 ->setWarningCount($faker->randomElement([0, 1, 2, 3]))
                 ->setIsBanned($faker->boolean(80))
                 ->setIsActive($faker->boolean(10))
