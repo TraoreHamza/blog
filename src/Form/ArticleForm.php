a815d1df84141d982e761a630bf78d613e5f323c
hamza <traorehamza407@gmail.com>
2025-06-04 14:58:39 +0200

    Suppression de l'importation inutile de EntityType dans ArticleForm.php

--- src/Form/ArticleForm.php
+++ src/Form/ArticleForm.php
@@ -7,7 +7,6 @@ use App\Entity\Block;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
