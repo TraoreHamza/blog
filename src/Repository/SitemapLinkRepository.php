9e63dc6712d94c284a6be00a26ea660fd2ea47c3
hamza <traorehamza407@gmail.com>
2025-06-04 14:58:55 +0200

    Correction de l'orthographe dans le nom de la classe SiteMapLinkRepository

--- src/Repository/SitemapLinkRepository.php
+++ src/Repository/SitemapLinkRepository.php
@@ -9,7 +9,7 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<SitemapLink>
  */
-class SitemapLinkRepository extends ServiceEntityRepository
+class SiteMapLinkRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
