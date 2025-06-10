e8546fd4a63fd2c3a5421179c3f8a1c3631cd689
hamza <traorehamza407@gmail.com>
2025-06-10 10:29:39 +0200

    Ajout de la gestion des slugs lors de la création et de la modification des articles, ainsi que le traitement des images.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Form\ArticleForm;
 use App\Repository\ArticleRepository;
 use App\Service\UploadService;
+use Cocur\Slugify\Slugify;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -47,7 +48,12 @@ final class ArticleController extends AbstractController
         // Traitement du formulaire
         if ($form->isSubmitted() && $form->isValid()) {
             $article->setAuthor($this->getUser()); // Récupération de l'utilisateur
-            
+
+            // Création du slug
+            $slugify = new Slugify();
+            $article->setSlug($slugify->slugify($article->getTitle()));
+
+            // Traitement de l'image
             if ($image = $form->get('image')->getData()) {
                 $article->setImage($us->upload($image, 'image'));
             }
@@ -76,7 +82,7 @@ final class ArticleController extends AbstractController
 
         if (!$article->isPublished()) {
             if ($article->getAuthor() !== $this->getUser()) {
-                $this->addFlash('error', "L'article n'est pas accessible pour le moment."); 
+                $this->addFlash('error', "L'article n'est pas accessible pour le moment.");
                 return $this->redirectToRoute('articles');
             }
         }
@@ -88,7 +94,7 @@ final class ArticleController extends AbstractController
 
     // Route "/article/{slug}/edit" menant à la modification d'un article
     #[Route('/{slug}/edit', name: 'article_edit', methods: ['GET', 'POST'])]
-    public function edit(string $slug, Request $request): Response
+    public function edit(string $slug, Request $request, UploadService $us): Response
     {
         $article = $this->ar->findOneBySlug($slug); // Récupération de l'article
 
@@ -103,6 +109,18 @@ final class ArticleController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) // Si le form est soumis et valide
         {
             try {
+                // traitement de l'image
+                if ($image = $form->get('image')->getData()) {
+                    $us->delete($article->getImage(), 'image'); // Supression de l'image existante
+                    $article->setImage($us->upload($image, 'image'));
+                }
+
+                // Traitement du slug si il a changé
+                if ($slug = $form->get('slug')->getData() !== $article->getSlug()) {
+                    $slugify = new Slugify();
+                    $article->setSlug($slugify->slugify($slug));
+                }
+
                 $this->em->persist($article); // Enregistrement de l'article (query SQL)
                 $this->em->flush($article); // Exécution de l'enregistrement en BDD
                 $this->addFlash('success', 'Modification bien prise en compte'); // Message Flash Success
@@ -148,19 +166,19 @@ final class ArticleController extends AbstractController
     }
 
     // Route "/article/{slug}/archive" pour publier un article
-    #[Route('/{slug}/archive', name: 'article_archive', methods: ['GET'])]
-    public function archive(string $slug): Response
-    {
+    // #[Route('/{slug}/archive', name: 'article_archive', methods: ['GET'])]
+    // public function archive(string $slug): Response
+    // {
 
-        // Récupérer l'article
-        // Vérifier que l'article existe
-        // Vérifier que l'article est archivé
-        // OUI : Le désarchiver
-        // NON : L'archiver
-        // Enregistrer les modifications
-        // Rediriger vers l'article
+    //     // Récupérer l'article
+    //     // Vérifier que l'article existe
+    //     // Vérifier que l'article est archivé
+    //     // OUI : Le désarchiver
+    //     // NON : L'archiver
+    //     // Enregistrer les modifications
+    //     // Rediriger vers l'article
 
-    }
+    // }
 
     // Route "/article/{slug}/status" pour publier ou archiver un article
     #[Route('/{slug}/status', name: 'article_status', methods: ['GET'])]
@@ -175,7 +193,7 @@ final class ArticleController extends AbstractController
 
         $action = $request->query->get('s');
 
-        if($action === 'publish') {
+        if ($action === 'publish') {
             $article->isPublished() ? $article->setIsPublished(false) : $article->setIsPublished(true);
         } else if ($action === 'archive') {
             $article->setIsArchived(!$article->isArchived());
