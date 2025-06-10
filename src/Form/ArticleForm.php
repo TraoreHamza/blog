67660879396ee993be7179efa81de571e4eef275
hamza <traorehamza407@gmail.com>
2025-06-10 10:29:52 +0200

    Ajout du champ 'slug' et amélioration des attributs des champs dans le formulaire d'article

--- src/Form/ArticleForm.php
+++ src/Form/ArticleForm.php
@@ -12,26 +12,37 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\UX\Dropzone\Form\DropzoneType;
 
 class ArticleForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class,[])
-            ->add('slug', TextType::class,[])
+            ->add('title', TextType::class, [
+                'attr' => ['placeholder' => "Titre de l'article"],
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
-                'required' => false
             ])
-            ->add('image', FileType::class, [
+            ->add('image', DropzoneType::class, [
+                'attr' => [
+                    'placeholder' => 'Sélectionnez une image',
+                    'class' => 'mb-4'
+                ],
                 'mapped' => false,
                 'required' => false
             ])
-            ->add('keywords', TextType::class,[])
-            ->add('description', TextareaType::class, [])
-            ->add('content', TextareaType::class, [])
+            ->add('keywords', TextType::class, [
+                'attr' => ['placeholder' => "Choisissez des mots-clés"],
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['placeholder' => "Résumé de l'article"],
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => ['placeholder' => "Rédigez votre article ici"],
+            ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregister'
             ])
