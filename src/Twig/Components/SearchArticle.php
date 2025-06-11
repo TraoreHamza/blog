1376e29f60150050343112d28f3927f9f56dfd19
hamza <traorehamza407@gmail.com>
2025-06-11 12:30:14 +0200

    Amélioration de la documentation et simplification du constructeur dans le composant SearchArticle

--- src/Twig/Components/SearchArticle.php
+++ src/Twig/Components/SearchArticle.php
@@ -12,18 +12,25 @@ final class SearchArticle
 {
     use DefaultActionTrait;
 
+    /**
+     * LiveProp est une classe qu'on utilise en annotation pour définir les propriétés "Live"
+     * que l'on va utiliser dans le composant. C'est comme le passage de props en JavaScript.
+     * 
+     * "writable : true" signifie que la propriété est modifiable depuis le composant.
+     * "url : true" signifie que la propriété sera disponible dans l'URL.
+     */
     #[LiveProp(writable: true, url: true)]
     public ?string $query = null;
 
-    public function __construct(private ArticleRepository $ar)
-    {
-    }
+    public function __construct(private ArticleRepository $ar) {}
 
     public function getArticles(): array
-    {
-        if($this->query) { // S'il y a une requête, on cherche les articles correspondants
-            return $this->ar->findAll($this->query);
+    {   
+        if ($this->query) { // S'il y a une requête, on cherche les articles correspondants
+            return $this->ar->searchByTitle($this->query);
         }
-        return $this->ar->findBy([], ['created_at' => 'DESC'], 10); // Sinon, on retourne les articles publiés les plus récents
+
+        // On retourne les 10 derniers articles par défaut
+        return $this->ar->findBy([], ['created_at' => 'DESC'], 10);
     }
 }
