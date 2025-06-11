053210d33a6d6975a96d563765abaddbb57449b5
hamza <traorehamza407@gmail.com>
2025-06-11 12:27:18 +0200

    Ajout d'une nouvelle ligne à la fin des fichiers User.php et UserFixtures.php pour respecter les conventions de formatage

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,7 +25,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 180)]
+    #[ORM\Column(length: 180, nullable: true)]
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
